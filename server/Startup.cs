4a5378de481ee5aca70ddde81a00a398dbae97cf
Tomaž Šuen <tomas.suen@gmail.com>
2017-07-26 10:40:22 +0200

    connected client to azure, minor changes to UI

--- server/Startup.cs
+++ server/Startup.cs
@@ -37,8 +37,13 @@ namespace WebApi
             services.AddCors();
             services.AddEntityFrameworkSqlServer()
                 .AddDbContext<DataContext>(x => x.UseSqlServer(
-                @"Data Source=DESKTOP-BP7LGIN;Initial Catalog=examco6;
-                Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" ));
+                /*@"Data Source=DESKTOP-BP7LGIN;Initial Catalog=examco6;
+                Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" /**/ 
+                @"Server=tcp:examco.database.windows.net,1433;Initial Catalog=examco1;
+                Persist Security Info=False;User ID='OsloAdmin';Password='Here we go1!';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                
+                ));
+                
             services.AddMvc();
             services.AddAutoMapper();
 

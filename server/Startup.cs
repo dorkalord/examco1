a01d96ac90f9401ddb0e60ac2f4c5c7997c5b050
dorkalord <tomas.suen@gmail.com>
2017-08-03 09:41:39 +0200

    Update Startup.cs

--- server/Startup.cs
+++ server/Startup.cs
@@ -39,9 +39,7 @@ namespace WebApi
                 .AddDbContext<DataContext>(x => x.UseSqlServer(
                 @"Data Source=DESKTOP-BP7LGIN;Initial Catalog=examco8;
                 Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" /**/ 
-                /*@"Server=tcp:examco.database.windows.net,1433;Initial Catalog=examco1;
-                Persist Security Info=False;User ID='OsloAdmin';Password='Here we go1!';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-                */
+                
                 ));
                 
             services.AddMvc();
